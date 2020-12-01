a5d062832e06e5661bfa5ce1fff4129ab48ff368
Mohsin Shishir <mohsinshishir09@gmail.com>
2020-12-02 01:55:46 +0600

    dashboard page modified...

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -15,52 +15,53 @@
         <div class="box box-primary">
             <div class="box-body">
 
-                <div class="container">
-                    <div class="row">
-                        <div class="four col-md-3">
+                <div class="container dashboard-container">
+                    <div class="row dashboard-row">
+                        <div class="four col-md-3 col-box">
                             <div class="counter-box "> <i class="fas fa-envelope"></i> <span class="counter" id="seats">2147</span>
                                 <p>৩৫০টি সংসদীয় আসনের মধ্যে
                                     ল্যাবের তালিকা পাঠানো আসন সংখ্যা </p>
                             </div>
                         </div>
-                        <div class="four col-md-3">
+                        <div class="four col-md-3 col-box">
                             <div class="counter-box"> <i class="fas fa-envelope-open"></i> <span class="counter">3275</span>
                                 <p>৩৫০টি সংসদীয় আসনে
                                     প্রাপ্ত আবেদন সংখ্যা </p>
                             </div>
                         </div>
-                        <div class="four col-md-3">
+                        <div class="four col-md-3 col-box">
                             <div class="counter-box"> <i class="fas fa-inbox"></i> <span class="counter">289</span>
                                 <p>৫০টি সংরক্ষিত মহিলা আসনের মধ্যে
                                     ল্যাবের তালিকা পাঠানো আসন সংখ্যা</p>
                             </div>
                         </div>
-                        <div class="four col-md-3">
+                        <div class="four col-md-3 col-box">
                             <div class="counter-box"> <i class="fas fa-envelope-open"></i> <span class="counter">1563</span>
                                 <p>৫০টি সংরক্ষিত মহিলা আসনে
                                     প্রাপ্ত আবেদন সংখ্যা </p>
                             </div>
                         </div>
                     </div>
-                    <div class="row">
-                        <div class="four col-md-3">
+
+                    <div class="row dashboard-row">
+                        <div class="four col-md-3 col-box">
                             <div class="counter-box "> <i class="fas fa-robot"></i> <span class="counter">2147</span>
                                 <p>৩০০টি স্কুল অফ ফিউচারের জন্য
                                     প্রাপ্ত আবেদন সংখ্যা </p>
                             </div>
                         </div>
-                        <div class="four col-md-3">
+                        <div class="four col-md-3 col-box">
                             <div class="counter-box"> <i class="fas fa-file-alt"></i><span class="counter">3275</span>
                                 <p>অন্যান্য রেফারেন্সের মাধ্যমে
                                     প্রাপ্ত আবেদন সংখ্যা </p>
                             </div>
                         </div>
-                        <div class="four col-md-3">
+                        <div class="four col-md-3 col-box">
                             <div class="counter-box"> <i class="fas fa-clipboard-list"></i><span class="counter">289</span>
                                 <p>মাননীয় এমপি মহোদয়ের মাধ্যমে ল্যাবের জন্য তালিকা গ্রহণ সম্পন্ন হওয়া জেলার সংখ্যা </p>
                             </div>
                         </div>
-                        <div class="four col-md-3">
+                        <div class="four col-md-3 col-box">
                             <div class="counter-box"> <i class="fas fa-mail-bulk"></i> <span class="counter">1563</span>
                                 <p>মোট প্রাপ্ত
                                     আবেদন সংখ্যা </p>
